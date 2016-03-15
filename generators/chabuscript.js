e424eed69ae3409a8daa981e854ab409be2dbb2e
montselozanod <montse_967@hotmail.com>
2016-03-15 11:43:17 -0600

    add and start generator for chabuscript to parse blocks

---
+++ generators/chabuscript.js
@@ -0,0 +1,16 @@
+/**
+ * @fileoverview Helper functions for generating Chabuscript for blocks.
+ * @author mmontse.lozano@google.com (Montse Lozano Dieck)
+ */
+
+ 'use strict';
+
+ goog.provide('Blockly.Chabuscript');
+
+ goog.require('Blockly.Generator');
+
+ /**
+  * Chabuscript code generator.
+  * @type {!Blockly.Generator}
+  */
+ Blockly.Chabuscript = new Blockly.Generator('Chabuscript');
