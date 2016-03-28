e14b70cbbf117e0678000bf4fe7b571612a21e0a
ilianagh <ghiliana@gmail.com>
2016-03-27 23:50:44 -0600

    logic blocks chabuscript

--- generators/chabuscript/logic.js
+++ generators/chabuscript/logic.js
@@ -1,45 +1,45 @@
-Blockly.JavaScript['logic_if'] = function(block) {
-  var value_if = Blockly.JavaScript.valueToCode(block, 'IF', Blockly.JavaScript.ORDER_ATOMIC);
-  var statements_do = Blockly.JavaScript.statementToCode(block, 'DO');
-  // TODO: Assemble JavaScript into code variable.
+Blockly.ChabuScript['logic_if'] = function(block) {
+  var value_if = Blockly.ChabuScript.valueToCode(block, 'IF', Blockly.ChabuScript.ORDER_ATOMIC);
+  var statements_do = Blockly.ChabuScript.statementToCode(block, 'DO');
+  // TODO: Assemble ChabuScript into code variable.
   var code = '...;\n';
   return code;
 };
 
-Blockly.JavaScript['logic_if_else'] = function(block) {
-  var value_if = Blockly.JavaScript.valueToCode(block, 'IF', Blockly.JavaScript.ORDER_ATOMIC);
-  var statements_do = Blockly.JavaScript.statementToCode(block, 'DO');
-  var statements_name = Blockly.JavaScript.statementToCode(block, 'NAME');
-  // TODO: Assemble JavaScript into code variable.
+Blockly.ChabuScript['logic_if_else'] = function(block) {
+  var value_if = Blockly.ChabuScript.valueToCode(block, 'IF', Blockly.ChabuScript.ORDER_ATOMIC);
+  var statements_do = Blockly.ChabuScript.statementToCode(block, 'DO');
+  var statements_name = Blockly.ChabuScript.statementToCode(block, 'NAME');
+  // TODO: Assemble ChabuScript into code variable.
   var code = '...;\n';
   return code;
 };
 
-Blockly.JavaScript['logic_if_elsif_else'] = function(block) {
-  var value_if = Blockly.JavaScript.valueToCode(block, 'IF', Blockly.JavaScript.ORDER_ATOMIC);
-  var statements_do = Blockly.JavaScript.statementToCode(block, 'DO');
-  var value_elsif = Blockly.JavaScript.valueToCode(block, 'ELSIF', Blockly.JavaScript.ORDER_ATOMIC);
-  var statements_name = Blockly.JavaScript.statementToCode(block, 'NAME');
-  var statements_else = Blockly.JavaScript.statementToCode(block, 'ELSE');
-  // TODO: Assemble JavaScript into code variable.
+Blockly.ChabuScript['logic_if_elsif_else'] = function(block) {
+  var value_if = Blockly.ChabuScript.valueToCode(block, 'IF', Blockly.ChabuScript.ORDER_ATOMIC);
+  var statements_do = Blockly.ChabuScript.statementToCode(block, 'DO');
+  var value_elsif = Blockly.ChabuScript.valueToCode(block, 'ELSIF', Blockly.ChabuScript.ORDER_ATOMIC);
+  var statements_name = Blockly.ChabuScript.statementToCode(block, 'NAME');
+  var statements_else = Blockly.ChabuScript.statementToCode(block, 'ELSE');
+  // TODO: Assemble ChabuScript into code variable.
   var code = '...;\n';
   return code;
 };
 
-Blockly.JavaScript['boolean_compare_expression'] = function(block) {
-  var value_left = Blockly.JavaScript.valueToCode(block, 'left', Blockly.JavaScript.ORDER_ATOMIC);
+Blockly.ChabuScript['boolean_compare_expression'] = function(block) {
+  var value_left = Blockly.ChabuScript.valueToCode(block, 'left', Blockly.ChabuScript.ORDER_ATOMIC);
   var dropdown_compare = block.getFieldValue('compare');
-  var value_right = Blockly.JavaScript.valueToCode(block, 'right', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
+  var value_right = Blockly.ChabuScript.valueToCode(block, 'right', Blockly.ChabuScript.ORDER_ATOMIC);
+  // TODO: Assemble ChabuScript into code variable.
   var code = '...';
   // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
+  return [code, Blockly.ChabuScript.ORDER_NONE];
 };
 
-Blockly.JavaScript['boolean_expression'] = function(block) {
+Blockly.ChabuScript['boolean_expression'] = function(block) {
   var dropdown_flag = block.getFieldValue('flag');
-  // TODO: Assemble JavaScript into code variable.
+  // TODO: Assemble ChabuScript into code variable.
   var code = '...';
   // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
+  return [code, Blockly.ChabuScript.ORDER_NONE];
 };
