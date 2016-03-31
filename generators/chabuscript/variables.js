5c80f18b2b90abd3e731e588aa2f6787a16e12b4
montselozanod <montse_967@hotmail.com>
2016-03-31 00:48:51 -0600

    fix vars

--- generators/chabuscript/variables.js
+++ generators/chabuscript/variables.js
@@ -9,27 +9,27 @@
 
  goog.require('Blockly.Chabuscript');
 
- Blockly.ChabuScript['var'] = function(block) {
+ Blockly.Chabuscript['var'] = function(block) {
    var dropdown_type = block.getFieldValue('type');
    var text_var_id = block.getFieldValue('var_id');
-   // TODO: Assemble ChabuScript into code variable.
+   // TODO: Assemble Chabuscript into code variable.
    var code = '...;\n';
    return code;
  };
 
- Blockly.ChabuScript['var_list'] = function(block) {
+ Blockly.Chabuscript['var_list'] = function(block) {
    var dropdown_type = block.getFieldValue('type');
    var text_list_id = block.getFieldValue('list_id');
-   var value_set_list = Blockly.ChabuScript.valueToCode(block, 'set_list', Blockly.ChabuScript.ORDER_ATOMIC);
-   // TODO: Assemble ChabuScript into code variable.
+   var value_set_list = Blockly.Chabuscript.valueToCode(block, 'set_list', Blockly.Chabuscript.ORDER_ATOMIC);
+   // TODO: Assemble Chabuscript into code variable.
    var code = '...;\n';
    return code;
  };
 
- Blockly.ChabuScript['variables_exist_var'] = function(block) {
+ Blockly.Chabuscript['variables_exist_var'] = function(block) {
    var text_var_name = block.getFieldValue('var_name');
    // TODO: Assemble Chabuscript into code variable.
    var code = '...';
    // TODO: Change ORDER_NONE to the correct strength.
-   return [code, Blockly.ChabuScript.ORDER_NONE];
+   return [code, Blockly.Chabuscript.ORDER_NONE];
  };
