b080a7ab2dfe2e5e3957daabde66183a41c5fd81
montselozanod <montse_967@hotmail.com>
2016-03-31 22:37:05 -0600

    code generation math

--- generators/chabuscript/math.js
+++ generators/chabuscript/math.js
@@ -7,8 +7,7 @@ goog.require('Blockly.Chabuscript');
 Blockly.Chabuscript['assign'] = function(block) {
   var value_opizq = Blockly.Chabuscript.valueToCode(block, 'opIzq', Blockly.Chabuscript.ORDER_ATOMIC);
   var value_opder = Blockly.Chabuscript.valueToCode(block, 'opDer', Blockly.Chabuscript.ORDER_ATOMIC);
-  // TODO: Assemble Chabuscript into code variable.
-  var code = '...;\n';
+  var code = value_opizq + '='+value_opder+';';
   return code;
 };
 
@@ -16,20 +15,18 @@ Blockly.Chabuscript['term'] = function(block) {
   var value_opizq = Blockly.Chabuscript.valueToCode(block, 'opIzq', Blockly.Chabuscript.ORDER_ATOMIC);
   var dropdown_op = block.getFieldValue('op');
   var value_opder = Blockly.Chabuscript.valueToCode(block, 'opDer', Blockly.Chabuscript.ORDER_ATOMIC);
-  // TODO: Assemble Chabuscript into code variable.
-  var code = '...';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.Chabuscript.ORDER_NONE];
+  var code = value_opizq + ' '+ dropdown_op + ' '+value_opder;
+  return [code, Blockly.Chabuscript.ORDER_MULTIPLICATION];
 };
 
 Blockly.Chabuscript['exp'] = function(block) {
   var value_opizq = Blockly.Chabuscript.valueToCode(block, 'opIzq', Blockly.Chabuscript.ORDER_ATOMIC);
   var dropdown_op = block.getFieldValue('op');
   var value_opder = Blockly.Chabuscript.valueToCode(block, 'opDer', Blockly.Chabuscript.ORDER_ATOMIC);
-  // TODO: Assemble Chabuscript into code variable.
-  var code = '...';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.Chabuscript.ORDER_NONE];
+
+  var code = value_opizq + ' '+ dropdown_op + ' '+value_opder;
+
+  return [code, Blockly.Chabuscript.ORDER_ADDITION];
 };
 
 
@@ -45,8 +42,8 @@ Blockly.Chabuscript['random'] = function(block) {
 
 Blockly.JavaScript['paren'] = function(block) {
   var value_expression = Blockly.JavaScript.valueToCode(block, 'expression', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  // TODO: Change ORDER_NONE to the correct strength.
+
+  var code = '(' + value_expression + ')';
+
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
