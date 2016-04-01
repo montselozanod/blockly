c7cfc30a129fcfcbfb248d58369f6a04cc9ddc79
montselozanod <montse_967@hotmail.com>
2016-04-01 01:12:49 -0600

    fix

--- generators/chabuscript/variables.js
+++ generators/chabuscript/variables.js
@@ -24,8 +24,6 @@
 
  Blockly.Chabuscript['variables_exist_var'] = function(block) {
    var text_var_name = block.getFieldValue('var_name');
-   // TODO: Assemble Chabuscript into code variable.
    var code = text_var_name;
-   // TODO: Change ORDER_NONE to the correct strength.
    return [code, Blockly.Chabuscript.ORDER_NONE];
  };
