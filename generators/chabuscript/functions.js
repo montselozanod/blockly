f7f9218205da35ec39fe77d5ac5b9d94d0484e33
montselozanod <montse_967@hotmail.com>
2016-04-01 01:16:07 -0600

    up

--- generators/chabuscript/functions.js
+++ generators/chabuscript/functions.js
@@ -19,19 +19,19 @@ Blockly.Chabuscript['main'] = function(block) {
   var statements_maint_stmts = Blockly.Chabuscript.statementToCode(block, 'maint_stmts');
 
   var code = 'start {' + statements_maint_stmts + '} end';
-  return [code, Blockly.JavaScript.ORDER_NONE];
+  return [code, Blockly.Chabuscript.ORDER_NONE];
 };
 
 
-Blockly.JavaScript['param_block'] = function(block) {
+Blockly.Chabuscript['param_block'] = function(block) {
   var dropdown_param_type = block.getFieldValue('param_type');
   var text_param_name = block.getFieldValue('param_name');
-  // TODO: Assemble JavaScript into code variable.
+  // TODO: Assemble Chabuscript into code variable.
   var code = dropdown_param_type + ' ' + text_param_name+ ';';
   return code;
 };
 
-Blockly.JavaScript['return_stmt'] = function(block) {
+Blockly.Chabuscript['return_stmt'] = function(block) {
   var text_value = block.getFieldValue('value');
   var code = 'return ' + text_value + ';';
   return code;
