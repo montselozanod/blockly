4a3a9b63821c7d8c1fc96afa7603a25db91d696a
montselozanod <montse_967@hotmail.com>
2016-04-21 23:57:52 -0500

    lists start

--- generators/chabuscript/lists.js
+++ generators/chabuscript/lists.js
@@ -17,13 +17,17 @@
 
    if(varIsUnique(text_list_name) && funcIsUnique(text_list_name))
    {
-
-     //var size = parseInt(text_size);
-     //addLocalVar(text_list_name, type, address, 1, size);
-
+     var size = parseInt(text_size);
+     var type = checkListType(dropdown_type);
+     var startAddress = sumAddress(type, size); //sumar a direccion las casillas del size del arreglo
+     addLocalVar(text_list_name, type, startAddress, 1, size);
+     return '';
+   }else{
+     var message = String.format(errors['DUPLICATE_VARIABLE_NAME'], text_list_name);
+     printToShell(message, true);
    }
 
-   return '';
+
  };
 
  Blockly.Chabuscript['list_values'] = function(block) {
@@ -33,10 +37,15 @@
 
    if(varIsUnique(text_list_name) && funcIsUnique(text_list_name))
    {
-     addLocalVar(text_list_name, type, address, 1);
+     var type = checkListType(dropdown_param_type);
+     var startAddress = sumAddress(type, listElements); //sumar a direccion las casillas del size del arreglo
+     addLocalVar(text_list_name, type, startAddress, 1, listElements+1); // size is always +1, but index starts with zero
+     listElements = 0;
+     return '';
+   }else{
+     var message = String.format(errors['DUPLICATE_VARIABLE_NAME'], text_listname_name);
+     printToShell(message, true);
    }
-   listElements = 0;
-   return '';
  };
 
  Blockly.Chabuscript['list_item'] = function(block) {
@@ -44,24 +53,40 @@
   // REVIEW: que pasa con la , cuando es el ultimo item
   var code = text_list_item + ',';
 
-  listElements = listElements++;
-  return code;
+  listElements = listElements++; //es el index empezando desde cero
+  return '';
 };
 
 Blockly.Chabuscript['list_put'] = function(block) {
   var text_list_name = block.getFieldValue('list_name');
   var text_item = block.getFieldValue('item');
   var text_index = block.getFieldValue('index');
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
+
+  var index = parseInt(text_index);
+  if(validateListAccess(index, text_index, text_list_name))
+  {
+
+      //TODO check type of textItem and list type
+      var op = Operation.PUT;
+      quadruples.push([op, ])
+      return '';
+  }
 };
 
 
 Blockly.Chabuscript['remove_item'] = function(block) {
-  var text_list_item = block.getFieldValue('list_ITEM');
+  var text_list_name = block.getFieldValue('list_ITEM');
   var text_index = block.getFieldValue('index');
   // REVIEW: con gramatica
   var code = 'list' + text_list_item + 'remove' + text_index + ';';
-  return code;
+
+  var index = parseInt(text_index);
+  if(validateListAccess(index, text_index, text_list_name))
+  {
+    var op = Operation.REMOVE;
+    var address = varTable[text_list_name][1] + index;
+    quadruples.push([op, , address, null, null]);
+    return '';
+  }
+
 };
