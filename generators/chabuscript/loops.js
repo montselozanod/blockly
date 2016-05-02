6533759c609c454fc91af864f7e916904e630d91
montselozanod <montse_967@hotmail.com>
2016-05-02 01:21:26 -0500

    fix quad of while

--- generators/chabuscript/loops.js
+++ generators/chabuscript/loops.js
@@ -7,19 +7,18 @@ goog.require('Blockly.Chabuscript');
 
 Blockly.Chabuscript['loop_while'] = function(block) {
   //var code = 'repeat while(' + value_while_cond + ')' + statements_while_stmts + 'end;';
-
-  var jump_false = quadruples.length+1;
+  var jump_back = quadruples.length; // cuadruplo al que se debe de regresar en el while
 
   var value_while_cond = Blockly.Chabuscript.valueToCode(block, 'while_cond', Blockly.Chabuscript.ORDER_ATOMIC);
 
   if (value_while_cond.type == Type.BOOL) {
     var flag = pilaO.pop();
-    quadruples.push([Operation.GOTOF, flag, null, 0]);
-    var jump = quadruples.length-1;
+    quadruples.push([Operation.GOTOF, value_while_cond.address, null, 0]); //este si esta bien
+    var jump_false = quadruples.length-1;
 
     Blockly.Chabuscript.statementToCode(block, 'while_stmts');
 
-    quadruples.push([Operation.GOTO, null, null, jump]);
+    quadruples.push([Operation.GOTO, null, null, jump_back]);
     quadruples[jump_false][3] = quadruples.length;
 
     return '';
